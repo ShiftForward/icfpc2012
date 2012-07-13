96b2e15f70d1b55389e164fe8fda3c0b90a59ef9
Bytter <hugosf@fe.up.pt>
2012-07-13 18:42:49 +0100

    Mais testes...

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,9 @@
 // ****************************************************************
 
 object Main extends App {
-  val b = Board("#*. #\n# R\\#\n#####".split('\n'))
+//  val b = Board("#*. #\n# R\\#\n#####".split('\n'))
+  val b = Board("#R  #\n#* *#\n#* *#\n#####".split('\n'))
+
   val f = b.eval(Up()).eval(Down()).eval(Right()).eval(Left())
 
   println (f)
