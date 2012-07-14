071989f4870674743557b4572530fa0dc8d09303
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-07-14 15:47:40 +0100

    Update main class.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,14 +14,32 @@ object Main extends App {
   // val b = Board("#L#\n#*#\n#*#\n# #\n# #\n#R#\n#\\#\n###".split('\n'))
   // val b = Board("# #\n#L#\n#\\#\n#R#\n###".split('\n'))
 
-  val b = Board.create("#  #\n#  #\n#  #\n#R #\n# *#\n# *#".split('\n'), (-1, 2, 1))
+  //val b = Board.create("#  #\n#  #\n#  #\n#R #\n# *#\n# *#".split('\n'), (-1, 2, 1))
+  val b = Board("src/main/resources/map/contest1.map")
+  println(b)
+
+  val lambdas = b.allLambdas
+
+  var moves = Agent.visitNodes(lambdas, b)
 
   // val b = Board("#RL#\n#* #\n#\\ #\n#  #\n#  #".split('\n'))
 
-  b.printStatus()
+//  b.printStatus()
 
-  val moves = List(Wait(), MoveDown(), MoveDown(), Wait(), MoveUp(), MoveUp(), MoveRight(), MoveUp(), MoveUp(), MoveUp())
+//  val moves = List(Wait(), MoveDown(), MoveDown(), Wait(), MoveUp(), MoveUp(), MoveRight(), MoveUp(), MoveUp(), MoveUp())
   // val moves = List(MoveDown(), MoveDown(), MoveDown(), Wait(), Wait(), Wait(), Wait(), Wait(), Wait(), Wait())
 
-  moves.foldLeft(b) { (cb: Board, m: Opcode) => val r = cb.eval(m); r.printStatus(); r }
+  val resb = moves.foldLeft(b) { (cb: Board, m: Opcode) => cb.eval(m) }
+  if (resb.lambdas == resb.tLambdas) {
+    val movesToEnd = ShortestPathCalculator.shortestPath(resb.liftPosition, resb)
+    if (!movesToEnd.isEmpty)
+      moves ++= movesToEnd
+    else
+      moves ++= List(Abort())
+  } else {
+    moves ++= List(Abort())
+  }
+  println(resb)
+  moves.foreach(print)
+  println
 }
