ea1ff8e53a7e46f01d7db75790bae685db247f73
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-07-14 14:52:00 +0100

    Fix agent priorities.

--- src/main/scala/Agent.scala
+++ src/main/scala/Agent.scala
@@ -1,13 +1,20 @@
 object Agent {
+  def orderedDestinations(c: Coordinate, destinations: List[Coordinate]) = {
+    destinations.sortBy { coordinate => coordinate.distance(c) }
+  }
+
   def visitNodes(nodes: List[Coordinate], board: Board): List[Opcode] = {
     if (nodes.isEmpty)
       List()
     else {
       val ops = ShortestPathCalculator.shortestPath(nodes.head, board)
       if (ops.length > 0) {
+        println("Got from " + board.robotPos + " to " + nodes.head)
         val nextBoard = board.eval(ops)
-        ops ++ visitNodes(nodes.tail, nextBoard)
+        ops ++ visitNodes(orderedDestinations(nextBoard.robotPos, nodes.tail),
+                          nextBoard)
       } else {
+        println("Unable to get from " + board.robotPos + " to " + nodes.head)
         visitNodes(nodes.tail, board)
       }
     }
